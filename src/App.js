c25405e3a7824b9c805675da7fbe4f3ef5e729f4
Shri Ganesh Jha <realsgj@gmail.com>
2022-06-12 22:08:30 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Contact from './components/Contact';
+import Home from "./components/Home";
+import Navbar from './components/Navbar';
+import Services from './components/Services';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      <div>
+        <Router>
+          <Navbar />
+          <div className="App container" id="App">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+              <Route path="/contact" element={<Contact />}></Route>
+              <Route path="/services" element={<Services />}></Route>
+            </Routes>
+          </div>
+        </Router>
+      </div>
+      </>
+    )
+  }
 }
-
-export default App;
