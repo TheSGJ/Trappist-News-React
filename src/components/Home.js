409a4fe7b02c21d7f6b2040fdadfb66da1ce2982
Shri Ganesh Jha <realsgj@gmail.com>
2022-07-13 12:50:34 +0000

    Few Fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,83 +1,146 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
-
+import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
 export default class Home extends Component {
- 
-  constructor(){
+  static defaultProps = {
+    pageSize: 5,
+  }
+  static propTypes = {
+    pageSize: PropTypes.number
+  }
+  constructor() {
     super();
-    // console.log("Debugging Home component constructer!")
     this.state = {
       articles: [],
       loading: false,
       page: 1,
-    }
+    };
   }
-  async componentDidMount(){
-    // console.log("Debugging CDM!");
-    let url = "/sample_data.json";
-    
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&${this.props.apiKey}&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles: parsedData.articles});
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
   }
+  handleHomePreClick = async () => {
+    window.scrollTo(0, 0);
+    let url = `https://newsapi.org/v2/top-headlines?country=us&${this.props.apiKey}&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+    console.log("Previous Page");
+  };
+  handleHomeNxtClick = async () => {
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      window.scrollTo(0, 0);
+      let url = `https://newsapi.org/v2/top-headlines?country=us&${this.props.apiKey}&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        totalResults: parsedData.totalResults,
+        loading: false,
+      });
+      console.log("Next Page");
+    }
+  };
   render() {
     return (
-      
-        <section className="text-gray-600 body-font">
-          <div className="container px-5 py-24 mx-auto">
-            <div className="flex flex-wrap -m-4">
-              {this.state.articles.map((element)=>{
-                return <NewsItem key={element.url} title={element.title} description={element.description?element.description.slice(0,88):""} imgUrl={element.urlToImage?element.urlToImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRID8CAckRSwj7DyX1BfOOohtFSUnfcPE9hag&usqp=CAU"} newsUrl={element.url} />
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          {this.state.loading && <Spinner />}
+          <div className="flex flex-wrap -m-4">
+            {!this.state.loading &&
+              this.state.articles.map((element) => {
+                return (
+                  <NewsItem
+                    key={element.url}
+                    title={element.title}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : ""
+                    }
+                    imgUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRID8CAckRSwj7DyX1BfOOohtFSUnfcPE9hag&usqp=CAU"
+                    }
+                    newsUrl={element.url}
+                  />
+                );
               })}
-            </div>
-
           </div>
-          <div className="container">
-
+        </div>
+        <div className="container">
           <>
-  <a
-    href="/"
-    className="inline-flex items-center py-2 px-4 mr-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-  >
-    <svg
-      className="mr-2 w-5 h-5"
-      fill="currentColor"
-      viewBox="0 0 20 20"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        fillRule="evenodd"
-        d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-        clipRule="evenodd"
-      />
-    </svg>
-    Previous
-  </a>
-  <a
-    href="/"
-    className="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-  >
-    Next
-    <svg
-      className="ml-2 w-5 h-5"
-      fill="currentColor"
-      viewBox="0 0 20 20"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        fillRule="evenodd"
-        d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-        clipRule="evenodd"
-      />
-    </svg>
-  </a>
-</>
-
-
-          </div>
-        </section>
-
+            <button
+              disabled={this.state.page <= 1}
+              className="inline-flex items-center py-2 px-4 mr-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              onClick={this.handleHomePreClick}
+            >
+              <svg
+                className="mr-2 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              Previous
+            </button>
+            <button
+              disabled={
+                this.state.page + 1 >
+                Math.ceil(this.state.totalResults / this.props.pageSize)
+              }
+              className="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              onClick={this.handleHomeNxtClick}
+            >
+              Next
+              <svg
+                className="ml-2 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </>
+        </div>
+      </section>
     );
   }
 }
