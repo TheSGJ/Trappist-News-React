25c074c28a7fb7c53939d875a41f2202f69f7355
Shri Ganesh Jha <realsgj@gmail.com>
2022-08-21 15:35:17 +0000

    Changes for master branch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -128,7 +128,7 @@ export default class Navbar extends Component {
                 </Link>
               </li>
               {/* popular categories */}
-              {/* <li>
+              <li>
                 <Link to="/business"
                   onClick={this.clickNavClose}
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
@@ -175,7 +175,7 @@ export default class Navbar extends Component {
                 >
                   Entertainment
                 </Link>
-              </li> */}
+              </li>
               {/* end popular categories */}
               <li>
                 <Link to="/services"
