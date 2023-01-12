92c9045edac1dc4eb4822da8e445624734cf5d5b
Shri Ganesh Jha <60005430+TheSGJ@users.noreply.github.com>
2023-01-13 01:57:15 +0530

    Update PopularBlogs.jsx

--- src/components/PopularBlogs.jsx
+++ src/components/PopularBlogs.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 import PopBlogItem from "./PopBlogItem";
 import Spinner from "./Spinner";
-import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class PopularBlogs extends Component {
   constructor() {
@@ -10,15 +9,14 @@ export default class PopularBlogs extends Component {
     // console.log("Debugging Home component constructer!")
     this.state = {
       blogArticles: [],
-      loading: false,
-      page: 1,
+      loading: true,
     };
   }
   async componentDidMount() {
     // console.log("Debugging PopularBlogs CDM!");
     this.props.setProgress(10)
-    let blogApi = `https://techcrunch.com/wp-json/wp/v2/posts?per_page=${this.props.per_page}&context=embed&page=1`;
-    this.setState({ loading: false });
+    let blogApi = 'https://timesofindia.indiatimes.com/rssfeeds/-2128838597.cms?feedtype=sjson';
+    this.setState({ loading: true });
     this.props.setProgress(40)
     let result = await fetch(blogApi);
     let getResult = await result.json();
@@ -26,7 +24,7 @@ export default class PopularBlogs extends Component {
     // Loging Number Post Objects
     this.props.setProgress(100)
     this.setState({
-      blogArticles: getResult,
+      blogArticles: getResult.channel.item,
       totalBlogResult: plGetResult,
       
       loading: false,
@@ -35,21 +33,7 @@ export default class PopularBlogs extends Component {
   //  console.log(this.state.blogArticles.id !== null)
   }
 
-  fetchMoreData = async () => {
-    this.setState({page: this.state.page + 1})
-    let blogApi = `https://techcrunch.com/wp-json/wp/v2/posts?per_page=${this.props.per_page}&context=embed&page=${this.state.page + 1}`;
-    this.setState({ loading: true });
-    let result = await fetch(blogApi);
-    let getResult = await result.json();
-    
-    this.setState({
-      
-      blogArticles: this.state.blogArticles.concat(getResult),
-      
-      loading: false
-    })
- 
-  }
+  
   render() {
     return (
       <>
@@ -59,13 +43,7 @@ export default class PopularBlogs extends Component {
       <div className="container my-6 px-6 mx-auto">
   <section className="mb-32 text-gray-800 text-center">
         <h1 className="text-3xl font-bold text-center pb-2 mb-4">Latest Top Tech - Trappist News</h1>
-        {this.state.loading && <Spinner />}
-          <InfiniteScroll
-          dataLength={this.state.blogArticles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.blogArticles.id !== null}
-          loader={<Spinner/>}
-        >
+          {this.state.loading && <Spinner />}
           
           <div className="grid lg:grid-cols-3 gap-6 xl:gap-x-12">
             {this.state.blogArticles.map((element) => {
@@ -74,18 +52,18 @@ export default class PopularBlogs extends Component {
         <div className="relative block bg-white rounded-lg shadow-lg">
                 <PopBlogItem
                   key={element.id}
-                  blogTitle={element.title.rendered}
-                  blogDetail={element.excerpt.rendered}
-                  blogSource={element.parsely.meta.publisher.name}
+                  blogTitle={element.title?element.title:""}
+                  blogDetail={element.description?element.description:"...."}
+                  blogSource='TOI'
                   
-                  blogAuthor={element.parsely.meta.creator?" by "+element.parsely.meta.creator:""}
-                  blogDate={element.date}
+                  blogAuthor="Times Of India"
+                  blogDate={element.pubDate?element.pubDate:"???"}
                   blogImgUrl={
-                    element.jetpack_featured_media_url
-                      ? element.jetpack_featured_media_url
+                    element.enclosure.@url
+                      ? element.enclosure.@url
                       : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRID8CAckRSwj7DyX1BfOOohtFSUnfcPE9hag&usqp=CAU"
                   }
-                  blogUrl={element.link}
+                  blogUrl={element.link?element.link:"/"}
                 />
                 </div>
                 </div>
@@ -93,7 +71,6 @@ export default class PopularBlogs extends Component {
             })}
           </div>
 
-            </InfiniteScroll>
       </section>
         </div>
 
